33bf7cb8cd0c587505a6a9182ca3ca87b8115371
CatsArePanzers <kristianm2005@gmail.com>
2024-02-12 22:02:41 +0200

    Changed the state machine logic

--- main.gd
+++ main.gd
@@ -1,21 +1,34 @@
 extends Node2D
 
-var allies = Array()
-var enemies = Array()
+var allies := Array()
+var enemies := Array()
+var enemy_controllers := Dictionary()
 var camera_idx: int = 0
 
-@onready var state_machine: StateMachine = $EnemyController/StateMachine
+const EnemyControllerScene = preload("res://enemy_controller.tscn")
+
 @onready var map: NavigationRegion2D = $NavigationRegion2D
 
 func spawn_enemy():
 	var new_enemy: Character = preload("res://entities/character.tscn").instantiate()
+	
 	%PathFollow2D.progress_ratio = randf()
 	new_enemy.global_position = %PathFollow2D.global_position
+	
 	add_child(new_enemy)
+	
 	var rand_rotation = randf() * PI * 2
 	new_enemy.weapon.rotation 				 = rand_rotation
 	new_enemy.detection_zone.rotation = rand_rotation + PI/2
+	
 	enemies.append(new_enemy)
+	
+	var new_enemy_controller: StateMachine = EnemyControllerScene.instantiate()
+	add_child(new_enemy_controller)
+	
+	new_enemy_controller.set_character(new_enemy)
+	enemy_controllers[new_enemy] = new_enemy_controller
+	
 	new_enemy.died.connect(remove_dead_enemy)
 
 func spawn_ally():
@@ -29,16 +42,11 @@ func spawn_ally():
 	allies.append(new_ally)
 	new_ally.connect("died", remove_dead_ally)
 
-
-
 func _ready():
 	allies.push_front($Player)
 	spawn_enemy()
-	#spawn_ally()
 	spawn_enemy()
-	#spawn_ally()
 	spawn_enemy()
-	#spawn_ally()
 	
 	allies[0].get_camera().make_current()
 
@@ -55,21 +63,13 @@ func _unhandled_key_input(event):
 		switch_camera(allies[camera_idx - 1].get_camera(), allies[camera_idx].get_camera())	
 
 func _physics_process(delta):
-	if enemies.size() == 0:
-		state_machine.set_character(null)
-		return
-	
-	for enemy in enemies:
-		state_machine.set_character(enemy)
-		state_machine.update(delta)
-	
 	pass
-	
+
 func _process(_delta):
 	pass
 
 func _on_spawner_timeout():
-	spawn_enemy()
+	#spawn_enemy()
 	#spawn_ally()
 	pass
 	
@@ -87,4 +87,7 @@ func remove_dead_ally(dead_ally):
 	allies.pop_at(allies.find(dead_ally))
 
 func remove_dead_enemy(dead_enemy):
+	enemy_controllers[dead_enemy].queue_free()
+	enemy_controllers.erase(dead_enemy)
 	enemies.pop_at(enemies.find(dead_enemy))
+
